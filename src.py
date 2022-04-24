44b111fadc3e899d382783b0df2cc7ab2b7bfba5
s3202844 <s3202844@vuw.leidenuniv.nl>
2022-04-24 16:11:47 +0200

    muti threads

--- src.py
+++ src.py
@@ -3,30 +3,65 @@ import sys
 import cv2
 import time
 
+from threading import Thread, Lock
 from color_detection import *
+
+global obstacle, yaw, obstacle_lock, yaw_lock
+obstacle, yaw = 0, 0
+
+obstacle_lock = Lock()
+yaw_lock = Lock()
 
 if not os.path.exists("results/"):
     os.mkdir("results/")
 
-cap = cv2.VideoCapture("test/1.avi")
 
-
-if __name__ == "__main__":
+def detect_obstacle(cap, channels=CONTOURS_COMB, debug=False):
     i = 0
-    debug = False
-    if len(sys.argv) > 1 and sys.argv[1] == "1":
-        debug = True
+    global obstacle, obstacle_lock
     while True:
         ret, frame = cap.read()
         if ret:
             pre = time.time()
-            detect(frame, channels=CONTOURS_COMB, debug=debug)
+            detect(frame, channels=channels, debug=debug)
+            obstacle_lock.acquire()
+            obstacle = i
+            # print(obstacle)
+            obstacle_lock.release()
             if debug:
                 print((time.time()-pre)*1000)
                 i += 1
                 cv2.imwrite("results/"+str(i)+".png", frame)
-            if cv2.waitKey(100) & 0xFF == ord('q'):
-                break
+                if cv2.waitKey(100) & 0xFF == ord('q'):
+                    break
         else:
             break
     cap.release()
+
+
+def main():
+    global obstacle, obstacle_lock
+    while True:
+        time.sleep(0.01)
+        # obstacle_lock.acquire()
+        # print(obstacle)
+        # obstacle_lock.release()
+        # time.sleep(30)
+
+
+if __name__ == "__main__":
+    debug = False
+    if len(sys.argv) > 1 and sys.argv[1] == "1":
+        debug = True
+    cap = cv2.VideoCapture("test/1.avi")
+
+    main_thread = Thread(target=main)
+    cv_thread = Thread(target=detect_obstacle,
+                       args=(cap, CONTOURS_COMB, debug))
+
+    main_thread.start()
+    cv_thread.start()
+
+    main_thread.join()
+    cv_thread.join()
+
